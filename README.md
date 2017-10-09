e8b9f3d49d6e03a18becc07bfe78e71b366a48e0
Jacob Castello <jacobcastello@gmail.com>
2017-10-09 13:18:27 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+Excella Demo
+=========
+
+A testing/demo role.
+
+Requirements
+------------
+
+Requirements are listed in the Pipfile, but to use that, install [Pipenv](https://docs.pipenv.org/)
+
+
+Usage
+-----
+
+Run this with [Molecule](http://molecule.readthedocs.io/en/latest/) to grasp its full potential.
+
+```
+pipenv install
+molecule test
+```
+
+License
+-------
+
+BSD
+
---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,21 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+def test_dnsmasq_package(host):
+    dnsmasq_package = host.package('dnsmasq')
+
+    assert dnsmasq_package.is_installed
+    assert dnsmasq_package.version.startswith("2.76")
